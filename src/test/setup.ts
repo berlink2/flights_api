15f4ade98c222f422a5fc057f0a0f0ece36ff7f0
berlink2 <berlink.k2@gmail.com>
2020-08-23 21:41:05 +0700

    removed irrelevant comments

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
-import { Flight } from "../models/flight";
 
 beforeAll(async () => {
-  //const mongoUri = await mongo.getUri();
-  const mongoUri = "mongodb://127.0.0.1:27017/test-flights-api";
   await mongoose.connect(process.env.TEST_MONGO_URI as string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,13 +10,6 @@ beforeAll(async () => {
 
 afterEach(async () => {
   await mongoose.connection.db.collection("flights").deleteMany({});
-  // for (let collection of collections) {
-  //   await collection.deleteMany({});
-  // }
-  // const collections = await mongoose.connection.db.collections();
-  // for (let collection of collections) {
-  //   await collection.deleteMany({});
-  // }
 });
 
 afterAll(async () => {
